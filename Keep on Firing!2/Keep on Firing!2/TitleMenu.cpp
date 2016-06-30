9a82850ca3a2b37ce921ee2d7fb43d2745649808
dorisol1019 <dorisol@hotmail.co.jp>
2016-06-30 21:33:07 +0900

    Add key_Enter,Z cliced logic

--- Keep on Firing!2/Keep on Firing!2/TitleMenu.cpp
+++ Keep on Firing!2/Keep on Firing!2/TitleMenu.cpp
@@ -26,45 +26,60 @@ void TitleMenu::update()
 		start = end = selectIndex = 0;
 		stopwatch1.reset();
 	}
-	if (Input::KeyUp.clicked)
+	if (stopwatch1.isActive())
 	{
-		start = selectIndex;
-		end = selectIndex = static_cast<uint32>((selectIndex + texts.size() - 1) % texts.size());
-		stopwatch2.restart();
-	}
-	if (Input::KeyDown.clicked)
-	{
-		start = selectIndex++;
-		end = (selectIndex %= texts.size());
-		stopwatch2.restart();
-	}
 
+		if (Input::KeyUp.clicked)
+		{
+			start = selectIndex;
+			end = selectIndex = static_cast<uint32>((selectIndex + texts.size() - 1) % texts.size());
+			stopwatch2.restart();
+		}
+		if (Input::KeyDown.clicked)
+		{
+			start = selectIndex++;
+			end = (selectIndex %= texts.size());
+			stopwatch2.restart();
+		}
+		if (Input::KeyEnter.clicked || Input::KeyZ.clicked)
+		{
+			if (texts[selectIndex] == L"‚Í‚¶‚ß‚é") {
+				changeScene(L"Game");
+			}
+			if (texts[selectIndex] == L"‚¨‚í‚é") {
+				System::Exit();
+			}
+		}
+	}
 }
 
 void TitleMenu::draw() const
 {
-	Graphics2D::SetTransform(Mat3x2::Identity());
-	RoundRect(messageBox, 20).draw(Color(20, 30, 120));
 	const double t0 = Min(stopwatch1.ms() / 500.0, 1.0);
-	const double e0 = EaseOut(Easing::Quart, t0);
+	const double e0 = EaseOut(Easing::Quad, t0);
 
 	if (e0 > 0.0)
 	{
-		const int windowCenter = Window::Center().x;
+		{
+			const Vec2 center(Window::Center());
+			Graphics2D::SetTransform(Mat3x2::Translate(-center).scale(e0).translate(center));
+			RoundRect(messageBox, 20).draw(Color(20, 30, 120));
+		}
+		const int windowCenterX = Window::Center().x;
 
 		{
 			const double t = Min(stopwatch1.ms() / 300.0, 1.0);
 			const double y = 200 + EaseOut(start, end, Easing::Quart, t) * 50;
 
 			RectF(16 * e0, 16 * e0).setCenter(260, y).rotated(stopwatch1.ms() / 500.0).draw(AlphaF(0.9*e0));
-			RectF(180, 50).setCenter(windowCenter, y).drawShadow({ 0,0 }, 10, 0, { 1.0,0.2*e0 });
+			RectF(180, 50).setCenter(windowCenterX, y).drawShadow({ 0,0 }, 10, 0, { 1.0,0.2*e0 });
 		}
 
 		for (auto& i : step(texts.size()))
 		{
 			const double t = Saturate((static_cast<double>(stopwatch1.ms()) - i + 50) / 500.0);
 			const double e = EaseOut(Easing::Quart, t);
-			const Vec2 center(windowCenter, 200 + i * 50);
+			const Vec2 center(windowCenterX, 200 + i * 50);
 
 			Graphics2D::SetTransform(Mat3x2::Translate(-center).scale(e).translate(center));
 			font(texts[i]).drawCenter(center);
