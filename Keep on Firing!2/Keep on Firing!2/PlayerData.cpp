e78788ec6bf7d5253b3da5f1347fdc54909f75db
sofken <sofken@example.com>
2016-07-05 16:36:21 +0900

    Create PlayerData_Class Add GameScoreView

---
+++ Keep on Firing!2/Keep on Firing!2/PlayerData.cpp
@@ -0,0 +1,2 @@
+#include"PlayerData.h"
+
---
+++ Keep on Firing!2/Keep on Firing!2/PlayerData.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+struct playerData
+{
+	int hitPoint = 3;
+	int score = 0;
+	int highscore = 0;
+	int gotItem = 0;
+	int bullet = 10;
+
+
+
+};
